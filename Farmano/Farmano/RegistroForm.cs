beceec69c013d0ba21020ec4f74ae53d330f536d
Aldair Cruz Santiago <aldaircruz9898@gmail.com>
2026-05-27 07:12:35 -0600

    Se crea estructura inicial MVP y formularios Login/Registro

--- Farmano/Farmano/RegistroForm.cs
+++ Farmano/Farmano/RegistroForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Farmano
 {
-    public partial class Form1 : Form
+    public partial class RegistroForm : Form
     {
-        public Form1()
+        public RegistroForm()
         {
             InitializeComponent();
         }
